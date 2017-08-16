d048c0d8b8f006956ace7f2345730b2d3ef1b496
annie <aknguyen@us.ibm.com>
2017-08-16 16:45:56 -0500

    add support for callback for terminal

--- src/main/content/js/guides/testCallBack.js
+++ src/main/content/js/guides/testCallBack.js
@@ -8,7 +8,36 @@ var testCallBack = (function() {
         editor.addSaveListener(addFileToBrowser);
     };
 
+    var __setCommandPrompt = function(cmdPrompt) {        
+        console.log(" cmdPrompt callback ", cmdPrompt);        
+        var cmds = {};
+        
+        cmds.help = function () {
+            var output = "<div>" +
+            "<ul>" +
+            "<li><strong>help</strong> - display this help.</li>" +
+            "<li><strong>test1</strong> - displays a cmd test1.</li>" +
+            "<li><strong>test2</strong> - displays a cmd test2.</li>" +
+            "</ul></div>";
+            return output;
+        };
+
+        cmds.test1 = function (args) {          
+            return "cmd test1";        
+        };        
+        
+        cmds.test2 = function (args) {          
+            return "cmd test2";        
+        };        
+        console.log("setCmdPrompt for ", cmds);
+        console.log("cmdPrompt.getId() ", cmdPrompt.getId());
+        console.log("cmdPrompt.getStepName() ", cmdPrompt.getStepName());
+        console.log("cmdPromp.getDefaultCmds() ", cmdPrompt.getDefaultCmds());
+        cmdPrompt.setCmdPrompt(cmds);        
+    };
+
     return {
-        setEditor: __setEditor
+        setEditor: __setEditor,
+        setCommandPrompt: __setCommandPrompt
     }
 })();
