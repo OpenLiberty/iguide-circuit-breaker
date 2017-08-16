ab17a7c909186e81cc351b102d8b405e005e3624
lavena <lavena@us.ibm.com>
2017-08-16 16:39:08 -0500

    Add contentManager to interact between content types

--- src/main/content/js/guides/testCallBack.js
+++ src/main/content/js/guides/testCallBack.js
@@ -8,7 +8,26 @@ var testCallBack = (function() {
         editor.addSaveListener(addFileToBrowser);
     };
 
+    var __refreshFileBrowser = function(editor) {
+        var __addFileToBrowser = function() {
+            var stepName = editor.getStepName();
+            console.log("in addFileToBrowser");
+            var fileBrowsers = contentManager.getFileBrowsers(editor.getStepName());
+            if (fileBrowsers) {
+                var fileBrowser = fileBrowsers[0];
+                console.log("fileBrowser", fileBrowser);
+                var file = {name: editor.getFileName()};
+                //file.name = editor.getFileName();
+                fileBrowser.__addFileElement(file, null, false);
+            } else {
+                console.log("not able to locate a file browser");
+            }
+        };
+        editor.addSaveListener(__addFileToBrowser);
+    };
+
     return {
-        setEditor: __setEditor
+        setEditor: __setEditor,
+        refreshFileBrowser: __refreshFileBrowser
     }
 })();
