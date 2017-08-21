fe5f13fc3bf00d0bd0c8c84490f92d85fd3e5731
annie <aknguyen@us.ibm.com>
2017-08-21 16:26:41 -0500

    drop changes for tab-in/tab-out of terminal

--- src/main/content/js/guides/messages.js
+++ src/main/content/js/guides/messages.js
@@ -8,5 +8,9 @@ var messages = {
   "browserRefreshButton": "Reload sample browser page",
   "navigationButtons": "Guide navigation buttons",
   "prevButton": "Previous",
-  "nextButton": "Next"
+  "nextButton": "Next",
+  "cmdPromptSample": "Terminal window",
+  "cmdPromptHelpMessage": "Type \'help\' to get started.",
+  "cmdPromptInput": "Enter a command",
+  "cmdPromptDisplayHelp": "display this help"
 };
