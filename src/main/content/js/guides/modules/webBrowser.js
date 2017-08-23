fc596801470e56c3e410b4ebcf36b470a42f41e4
dmorgant <dmorgant@us.ibm.com>
2017-08-23 10:13:32 -0500

    Callback functionality added for webBrowser instances

--- src/main/content/js/guides/modules/webBrowser.js
+++ src/main/content/js/guides/modules/webBrowser.js
@@ -3,6 +3,8 @@ var webBrowser = (function(){
   var webBrowserType = function(container, stepName, content) {
     this.stepName = stepName;
     this.contentRootElement = null;
+    this.updatedURLCallback = null;    // User-defined callback function
+                                       // invoked when the URL is updated
 
     if (content.url) {
       this.webURL = content.url;
@@ -15,7 +17,8 @@ var webBrowser = (function(){
     } else {
       this.webContent = "";
     }
-    // Need a mapping of URLs to pages?
+    
+// Need a mapping of URLs to pages?
 
     __loadAndCreate(this, container, stepName, content);
   }
@@ -30,7 +33,7 @@ var webBrowser = (function(){
       this.contentRootElement.find('.wbNavURL').val(URLvalue);
     },
     __getURL:  function() {
-      return this.contentRootElement.find('.wbNavBar').val();
+      return this.contentRootElement.find('.wbNavURL').val();
     },
 
     __setBrowserContent: function(content) {
@@ -57,6 +60,16 @@ var webBrowser = (function(){
       } else {   
         webContentElement.html(content);
       }
+    },
+
+    // Registers a callback method with this webBrowser
+    // instance.  It will be invoked when the URL is updated
+    // or the Refresh button is selected and will receive the 
+    // navbar URL value as a parameter.  The function can 
+    // then identify the browser contents associated with the
+    // URL value.
+    addUpdatedURLListener: function(callback) {
+       this.updatedURLCallback = callback;
     }
 
   };
@@ -71,17 +84,28 @@ var webBrowser = (function(){
           container.append($(result));
           this.contentRootElement = container.find('.wb');
           $wbNavURL = this.contentRootElement.find('.wbNavURL');
+          $wbContent = this.contentRootElement.find('.wbContent');
+
+          // set aria labels
+          this.contentRootElement.attr('aria-label', messages.browserSample);
+          $wbNavURL.attr('aria-label', messages.browserAddressBar);
+          $wbContent.attr('aria-label', messages.browserContentIdentifier);
+          this.contentRootElement.find('.wbRefreshButton').attr('aria-label', messages.browserRefreshButton);
 
           // Select URL text when in focus
           $wbNavURL.focus(function() {
               $(this).select();
           });
 
-          // set aria labels
-          this.contentRootElement.attr('aria-label', messages.browserSample);
-          $wbNavURL.attr('aria-label', messages.browserAddressBar);
-          this.contentRootElement.find('.wbContent').attr('aria-label', messages.browserContentIdentifier);
-          this.contentRootElement.find('.wbRefreshButton').attr('aria-label', messages.browserRefreshButton);
+           if (content.callback) {
+            var callback = eval(content.callback);
+            // Identify this webBrowser with the updatedURLCallback
+            // function specified by the user.
+            callback(thisWebBrowser);
+          }
+
+          __addBrowserListeners(thisWebBrowser);
+
           // fill in contents
           this.__setURL(this.webURL);        
           this.__setBrowserContent(this.webContent);
@@ -90,6 +114,33 @@ var webBrowser = (function(){
           console.error("Could not load webBrowser.html");
         }
       });
+  };
+
+  var __addBrowserListeners = function(thisWebBrowser) {
+    var urlField = thisWebBrowser.contentRootElement.find('.wbNavURL');
+    urlField.on("keydown", function(event) {
+      if (event.which === 13) {  // Enter key
+        if (thisWebBrowser.updatedURLCallback) {
+          thisWebBrowser.updatedURLCallback(thisWebBrowser.__getURL());
+        }  // else, don't do anything.  This webBrowser instance does
+           // not support URL changes.
+      }
+    });
+
+    var refreshButton = thisWebBrowser.contentRootElement.find('.wbRefreshButton');
+    if (thisWebBrowser.updatedURLCallback) {
+      refreshButton.on("click", function(event) {
+        event.stopPropagation();
+        thisWebBrowser.updatedURLCallback(thisWebBrowser.__getURL());
+      });
+    } else {   // This webBrowser does not support URL changes.  Redisplay current HTML.
+      console.log(thisWebBrowser.webURL);
+      console.log(thisWebBrowser.webContent);
+      refreshButton.on("click", function(event) {
+        thisWebBrowser.__setURL(thisWebBrowser.webURL);
+        thisWebBrowser.__setBrowserContent(thisWebBrowser.webContent);
+      });
+    }
   };
 
   var __create = function(container, stepName, content) {
