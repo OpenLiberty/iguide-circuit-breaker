66fc7f7528e8fe53f75e4f23c7a6d77e45d9591d
dmorgant <dmorgant@us.ibm.com>
2017-09-01 14:21:10 -0500

    Update location of browser html files in webBrowser

--- src/main/content/js/guides/modules/webBrowser.js
+++ src/main/content/js/guides/modules/webBrowser.js
@@ -46,7 +46,7 @@ var webBrowser = (function(){
       var extension = content.substring(content.length - 4).toLowerCase();
       var file =  extension === 'html' || extension === 'htm' ? true: false;
       if (file) {
-        var fileLocation = '../js/guides/wbFiles/' + content;
+        var fileLocation = '../html/guides/' + content;
         var $iframe = $webContentElement.find('iframe');
         $iframe.attr('src', fileLocation);
 
