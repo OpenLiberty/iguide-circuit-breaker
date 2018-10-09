35b3f18b5deae4af7d15c6480d02c590f6d5ff5b
amyreit <amyreit@us.ibm.com>
2018-10-09 16:51:00 -0500

    Adding another callback string

--- js/interactive-guides/circuit-breaker/nls/messages.js
+++ js/interactive-guides/circuit-breaker/nls/messages.js
@@ -15,6 +15,8 @@ var messages = {
     CHOOSE_NEXT_REQUEST: "Choose whether the next request to the microservice succeeds or fails. Connection attempts are shown from most recent to oldest.  The brackets represent the rolling window.",
     WELCOME: "Welcome to Global eBank!",
     PAGENOTFOUND: "404 Page not found error",
-    OH_NO: "Oh no! The Check Balance microservice is down! As more requests come into the service&#44 the users notice that their check balance requests are taking much longer and seem to hang. The users repeatedly refresh the page&#44 stacking up the requests to the Check Balance microservice even further. Eventually&#44 the web application is so busy servicing the failed requests that it comes to a crawl&#44 even for those not using the Check Balance microservice." 
+    OH_NO: "Oh no! The Check Balance microservice is down! As more requests come into the service&#44 the users notice that their check balance requests are taking much longer and seem to hang. The users repeatedly refresh the page&#44 stacking up the requests to the Check Balance microservice even further. Eventually&#44 the web application is so busy servicing the failed requests that it comes to a crawl&#44 even for those not using the Check Balance microservice.",
+    CIRCUIT_REMAINS: "The circuit remains in an open state for <code>5000 ms</code> before switching to a <b>half-open</b> state.",
+    ASSUMING_CIRCUIT: "Assuming the circuit is in an <b>open</b> state&#44 the request to the Check Balance microservice fails immediately.  You are instantly notified of the problem and no longer must wait for the time-out period to occur to receive the notification." 
 } ;
  
