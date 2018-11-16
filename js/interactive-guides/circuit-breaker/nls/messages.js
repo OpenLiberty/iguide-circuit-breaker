e7cc019ff257c7524dd499981d4fbec388ee4c63
Denise Morganti <dmorgant@us.ibm.com>
2018-11-16 13:38:48 -0600

    Use welcome page from iguides-common

--- js/interactive-guides/circuit-breaker/nls/messages.js
+++ js/interactive-guides/circuit-breaker/nls/messages.js
@@ -2,7 +2,6 @@
 var circuit_breaker_messages = {
     ABOUT: "About",
     USER: "User",
-    BANK_LOGO: "Bank Logo",
     YOUR_ACCOUNT: "Global eBank - Your Account", 
     SYSTEM_DOWN: "The system is down.",
     TRY_AGAIN: "Try again later.",
@@ -18,7 +17,6 @@ var circuit_breaker_messages = {
     DELAY: "Delay&#58",
     DELAYJS: "Delay:",
     CHOOSE_NEXT_REQUEST: "Choose whether the next request to the microservice succeeds or fails. Connection attempts are shown from most recent to oldest.  The brackets represent the rolling window when the circuit is closed.",
-    WELCOME: "Welcome to ",
     PAGENOTFOUND: "404 Page not found error",
     OH_NO: "Oh no! The Check Balance microservice is down! As more requests come into the service&#44 the users notice that their check balance requests are taking much longer and seem to hang. The users repeatedly refresh the page&#44 stacking up the requests to the Check Balance microservice even further. Eventually&#44 the web application is so busy servicing the failed requests that it comes to a crawl&#44 even for those not using the Check Balance microservice.",
     CIRCUIT_REMAINS: "The circuit remains in an open state for <code>5000 ms</code> before switching to a <b>half-open</b> state.",
